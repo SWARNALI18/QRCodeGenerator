7976329321c3da5c16c04fa35d1313bd0ebb1925
unknown <swarnalibhattacharjee18@gmail.com>
2020-05-17 12:05:31 +0530

    first commit

---
+++ QRCodeGen.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter2;
+
+/**
+ *
+ * @author user
+ */
+//import com.sun.javafx.iio.ImageStorage.ImageType;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import net.glxn.qrgen.image.ImageType;
+import java.io.FileOutputStream;
+import net.glxn.qrgen.QRCode;
+public class QRCodeGen  {
+    public static void main(String[] args)throws Exception {
+        String Details="I am Swarnali with no Dream,no destiny,no aim,no goal.";
+        ByteArrayOutputStream out=QRCode.from(Details).to(ImageType.JPG).stream();
+        File f=new File("C:\\Users\\user\\Downloads\\MyChannel.jpg");
+        FileOutputStream fos=new FileOutputStream(f);
+        fos.write(out.toByteArray());
+        fos.flush();
+    }
+
+}
